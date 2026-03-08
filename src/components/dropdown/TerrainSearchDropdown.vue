3b0ebf3f0f40f0cd421056d1837438bf5dd3d924
fBlurryface <98239377+fBlurryface@users.noreply.github.com>
2026-03-09 05:31:05 +0800

    Create TerrainSearchDropdown.vue

---
+++ src/components/dropdown/TerrainSearchDropdown.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { Identifier } from "deepslate";
+import ListDropdown from "./ListDropdown.vue";
+import { useLoadedDimensionStore } from "../../stores/useLoadedDimensionStore.js";
+import {
+	useTerrainSearchStore,
+	type TerrainSearchToolId,
+} from "../../stores/useTerrainSearchStore.js";
+
+const terrainSearchStore = useTerrainSearchStore();
+const loadedDimensionStore = useLoadedDimensionStore();
+
+const entries = [Identifier.create("minecraft:ocean_monument")];
+
+function toggleTool(id: Identifier) {
+	const key = id.toString() as TerrainSearchToolId;
+
+	if (terrainSearchStore.tools.has(key)) {
+		terrainSearchStore.tools.delete(key);
+	} else {
+		terrainSearchStore.tools.add(key);
+	}
+
+	terrainSearchStore.$patch({});
+}
+
+function disableGroup(group: string) {
+	[...terrainSearchStore.tools].forEach((tool) => {
+		if (tool.startsWith(group + ":")) {
+			terrainSearchStore.tools.delete(tool);
+		}
+	});
+
+	terrainSearchStore.$patch({});
+}
+</script>
+
+<template>
+	<ListDropdown
+		:type="'structure'"
+		:placeholder="'Search terrain tools...'"
+		:entries="entries"
+		:icons="loadedDimensionStore.getIcon"
+		:selected="terrainSearchStore.tools"
+		@toggle="toggleTool"
+		@disableGroup="disableGroup"
+	/>
+</template>
+
+<style scoped>
+</style>
